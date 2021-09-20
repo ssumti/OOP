24c887c80fc5b25098d0a11dc46c0e28a0293045
ssumti <tudanchuy@gmail.com>
2021-09-20 13:49:32 +0700

    commit

---
+++ Testing/test/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace test
+{
+    class Program
+    {
+        bool isConnected = false;
+        public static async Task Main(string[] args)
+        {
+            HubConnection hub;
+            hub = new HubConnectionBuilder().WithUrl("https://localhost:44306/hubs/game").Build();
+            await hub.StartAsync();
+            await hub.SendAsync("AddToGroup", "Room1", "ssumti");
+            hub.Closed += async (error) =>
+            {
+                Random random = new Random();
+                await Task.Delay(random.Next(0, 5) * 1000);
+                await hub.StartAsync();
+                await hub.SendAsync("AddToGroup", "Room1", "ssumti");
+                Console.WriteLine("Connected...");
+            };
+            hub.On<string>("EnteredOrLeft", (message) =>
+            {
+                Console.WriteLine(message);
+            });
+            hub.On<string, string>("ReceiveMessage", (user, message) =>
+            {
+                Console.WriteLine(user + ": " + message);
+            });
+            string key = "a";
+            while (key != "")
+            {
+                key = Console.ReadLine();
+                // switch (key)
+                // {
+                //     case '1':
+                //         await hub.InvokeAsync("SendMessageGroup", "Room1", "ssumti", "HelloThere");
+                //         break;
+                //     default:
+                //         key = '0';
+                //         break;
+                // }
+                await hub.InvokeAsync("SendMessageGroup", "Room1", "ssumti", key);
+            }
+            await hub.SendAsync("RemoveFromGroup", "Room1", "ssumti");
+        }
+    }
+
+}
